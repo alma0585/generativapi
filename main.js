9d8de18acecc4d056d9d46c2212359cb2524a7ce
alma0585 <alma.castillo028@gmail.com>
2024-11-24 13:38:12 +0100

    første commmit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const OPENAI_API_KEY = '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';
 const princessDisplay = document.querySelector(".generated-princess p");
 
 // Funktion til at generere prinsesseteksten.
